7ceb0feab725752ea42de63d484116490df6feb4
NotUnlikeTheWaves <hleistra@gmail.com>
2021-03-05 22:12:21 +0100

    Figure out spacing and centering horizontally

--- web/src/App.js
+++ web/src/App.js
@@ -5,10 +5,12 @@ import React from 'react'
 const apiEndpoint = "http://localhost:8080"
 
 function Document(props) {
-  return <div class="border-solid border-4 border-blue-500 bg-red-500">
+  return <div class="block border-solid border-4 border-blue-500 hover:bg-yellow-500 bg-red-500 px-2 py-2">
     <img src={apiEndpoint + props.Url} alt={props.Name} />
 
-    {props.Name}
+    <div class="text-yellow-400 text-right">
+      {props.Name}
+    </div>
   </div>
 }
 
@@ -44,11 +46,15 @@ class Base extends React.Component {
     if (this.state.docsLoaded === false) {
       return <h1>Nothing yet!</h1>
     } else {
-      return <div>
-        <h1>Hello</h1>
-        <div class="w-9/12 grid grid-cols-3 gap-4">{this.state.docs["files"].map((m, i) => {
-          return <div key={i}>{Document(m)}</div>
-        })}</div>
+      return <div class="flex flex-col items-center">
+        <div class="block">Tagger</div>
+          <div class="w-9/12 grid grid-cols-3 gap-4 py-10">
+            {
+              this.state.docs["files"].map((m, i) => {
+                return <div key={i}>{Document(m)}</div>
+              })
+            }
+          </div>
         </div>
     }
   }
