af2fa960b774ba42cc9d20cf9474d06f099370ed
NotUnlikeTheWaves <hleistra@gmail.com>
2021-03-19 00:53:34 +0100

    Change styling now snoooooooooooooooooze

--- web/src/Content.js
+++ web/src/Content.js
@@ -46,12 +46,12 @@ class Content extends React.Component {
   }
 
   const Document = (props) => {
-    return <div class="block border-solid border-4 border-blue-500 hover:bg-yellow-500 bg-red-500  px-2 py-2">
+    return <div class="block border-solid rounded border-4 border-blue-500 hover:bg-yellow-500 bg-gray-500  px-2 py-2">
       <div class="flex flex-rows space-x-400">
         <div>
           <img src={apiEndpoint + props.Url} alt={props.Name} />
       
-          <div class="text-yellow-400 text-right">
+          <div class="text-yellow-400 text-right underline">
             {props.Name}
           </div>
         </div>
