1bfbf91f83916c26c960347ed967900f8398e9fd
NotUnlikeTheWaves <hleistra@gmail.com>
2021-03-18 23:28:57 +0100

    Add modal base

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Content from './Content'
 import TagOverview from './Tags'
+import Modal from './Modal'
 
 const App = () => {
-  return <div class="bg-gray-600">
+  return (
+    <div class="bg-gray-600">
       <div class="flex flex-col items-center space-y-4 mx-auto w-9/12 py-10">
         <div>Hello, this is Tagger</div>
         <TagOverview />
         <Content />
+        <div>
+          <form class="bg-blue-100 border rounded py-1 px-1 ">
+          # <input type="text" class="bg-blue-100 w-4/12"/>
+          </form>
+        </div>
+        <Modal />
       </div>
     </div>
+  )
 }
 
 export default App
