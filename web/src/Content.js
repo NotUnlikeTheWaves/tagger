346c6a93bc9ed253aecb9c4c49ea5a4ea88f9984
NotUnlikeTheWaves <hleistra@gmail.com>
2021-03-20 01:46:22 +0100

    Center images somehow

--- web/src/Content.js
+++ web/src/Content.js
@@ -51,8 +51,8 @@ class Content extends React.Component {
   const Document = (props) => {
     return <div class="block border-solid rounded border-4 border-blue-600 hover:bg-yellow-500 bg-gray-500 px-2 py-2">
       <div class="flex flex-rows gap-2">
-        <div>
-          <img src={apiEndpoint + props.Url} alt={props.Name} />
+        <div class="w-2/3">
+          <img class="mx-auto" src={apiEndpoint + props.Url} alt={props.Name} />
       
           <div class="text-yellow-400 text-right underline">
             {props.Name}
