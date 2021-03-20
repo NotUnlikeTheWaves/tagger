5a99b1e34584e27bdb4adb707c2c76acd6a764a7
NotUnlikeTheWaves <hleistra@gmail.com>
2021-03-20 02:15:16 +0100

    Restructure the tag list a bit for reuse

--- web/src/Content.js
+++ web/src/Content.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import apiEndpoint from './Api'
-import { RenderTag } from './Tags'
-import Modal from './Modal'
+import { RenderTag, RenderTagList } from './Tags'
+import EditTags from './EditTags'
 
 class Content extends React.Component {
     constructor(props) {
@@ -38,8 +38,14 @@ class Content extends React.Component {
         return (
           <div class="grid grid-cols-3 gap-4">
             {
-              this.state.docs["files"].map((m, i) => {
-                return <div key={i}>{Document(m)}</div>
+              this.state.docs["files"].map((document, i) => {
+                return (
+                    <div key={i}>
+                      <div class="block border-solid rounded border-4 border-blue-600 hover:bg-yellow-500 bg-gray-500 px-2 py-2">
+                        {Document(document, false)}
+                      </div>
+                    </div>
+                   )
               })
             }
           </div>
@@ -48,40 +54,30 @@ class Content extends React.Component {
     }
   }
 
-  const Document = (props) => {
-    return <div class="block border-solid rounded border-4 border-blue-600 hover:bg-yellow-500 bg-gray-500 px-2 py-2">
+  const Document = (document, isEdit) => {
+    return (
       <div class="flex flex-rows gap-2">
         <div class="w-2/3">
-          <img class="mx-auto" src={apiEndpoint + props.Url} alt={props.Name} />
+          <img class="mx-auto" src={apiEndpoint + document.Url} alt={document.Name} />
       
           <div class="text-yellow-400 text-right underline">
-            {props.Name}
+            {document.Name}
           </div>
         </div>
         {/* Place content between seems a bit hacky but gives me the result I want */}
-        <div class="flex items-stretch place-content-between gap-2 border-l border-blue-600 flex-col pl-2">
-          <div class="flex flex-row">
-          {
-              props.Tags.filter(m => m.Hidden == false).map((m, i) => {
-                          // return m.Name
-                            return <div key={i}>{RenderTag(m)}</div>
-                        })
-          }
-          </div>
-          <div class="flex flex-row">
-          {
-              props.Tags.filter(m => m.Hidden == true).map((m, i) => {
-                          // return m.Name
-                            return <div key={i}>{RenderTag(m)}</div>
-                        })
-          }
-          </div>
+        <div class="w-1/3 flex items-stretch place-content-between gap-2 border-l border-blue-600 flex-col pl-2">
           <div class="">
-          <Modal />
+          {
+            RenderTagList(document.Tags, false)
+          }
           </div>
+          {
+            !isEdit && <div class="">
+              <EditTags document={document} />
+            </div>
+          }
         </div>
-      </div>
-    </div>
+      </div>)
   }
   
 
--- web/src/Tags.js
+++ web/src/Tags.js
@@ -33,26 +33,31 @@ class TagOverview extends React.Component {
         if(this.state.tagsLoaded === false) {
             return <div>There are no loaded tags yet!</div>
         } else {
-            return (
-            <div class="flex flex-col items-center gap-2">
-                <div class="flex flex-row">
-                    {
-                        this.state.tags["tags"].filter(tag => tag.Hidden == false).map((m, i) => {
-                            return <div key={i}>{RenderTag(m)}</div>
-                        })
-                    }
-                </div>
-                <div class="flex flex-row">
-                    {
-                        this.state.tags["tags"].filter(tag => tag.Hidden == true).map((m, i) => {
-                            return <div key={i}>{RenderTag(m)}</div>
-                        })
-                    }
-                </div>
-            </div>
-            )
+            return RenderTagList(this.state.tags["tags"], true)
         }
     }
+}
+
+const RenderTagList = (tags, center, selectCallBack) => {
+    var align = center == true ? "items-center" : "items-left"
+    return (
+        <div class={"flex flex-col " + align + " gap-2"}>
+            <div class="flex flex-row mr-0">
+                {
+                    tags.filter(tag => tag.Hidden == false).map((m, i) => {
+                        return <div key={i}>{RenderTag(m)}</div>
+                    })
+                }
+            </div>
+            <div class="flex flex-row">
+                {
+                    tags.filter(tag => tag.Hidden == true).map((m, i) => {
+                        return <div key={i}>{RenderTag(m)}</div>
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 const RenderTag = (tag) => {
@@ -65,4 +70,4 @@ const RenderTag = (tag) => {
 }
 
 export default TagOverview
-export {TagOverview, RenderTag}
+export {TagOverview, RenderTag, RenderTagList}
