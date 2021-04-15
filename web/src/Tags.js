a5883826b456c5b4eedd198cd0d5d5a94adea82b
NotUnlikeTheWaves <hleistra@gmail.com>
2021-04-16 01:36:17 +0200

    Basic filtering of tags

--- web/src/Tags.js
+++ web/src/Tags.js
@@ -6,13 +6,24 @@ class TagOverview extends React.Component {
         super(props)
         this.state = {
             tags: [],
+            filter: [],
             tagsLoaded: false,
             error: null
         }
+        this.indexOfTagInFilter.bind(this)
     }
 
     componentDidMount() {
-        fetch(apiEndpoint + "/api/v1/tagList")
+        console.log("hey")
+        this.loadTagList()
+    }
+
+    loadTagList() {
+        console.log("state in load-tag-list:")
+        console.log(this.state.filter)
+        var query = this.createFilterQuery()
+        console.log("query is: " + query)
+        fetch(apiEndpoint + "/api/v1/tagList" + query)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -29,6 +40,51 @@ class TagOverview extends React.Component {
             )
     }
 
+    indexOfTagInFilter(tag) {
+        return this.state.filter.findIndex(v => v.Hidden === tag.Hidden &&
+            v.Name === tag.Name)
+    }
+
+    createFilterQuery() {
+        if(this.state.filter.length > 0) {
+            const parameters = this.state.filter.map(tag => "filter=" + (tag.Hidden ? 1 : 0) + "|" + tag.Name)
+            const query = "?" + parameters.join('&')
+            return query
+        }
+        return ""
+    }
+
+    empty() {
+        console.log("state in empty function")
+        console.log(this.state.filter)
+    }
+
+    switchFilter(tag) {
+        const index = this.indexOfTagInFilter(tag)
+        console.log("index is: " + index)
+        console.log("index check:")
+        console.log(this.state.filter)
+        console.log("tag:")
+        console.log(tag)
+        console.log("------")
+        if (index == -1) {
+            const filter = this.state.filter.concat(tag)
+            console.log("add filter:")
+            console.log(filter)
+            this.setState({
+                filter: filter
+            }, () => {this.loadTagList()})
+        } else {
+            const filter = this.state.filter
+            filter.splice(index, 1)
+            console.log("delete filter:")
+            console.log(filter)
+            this.setState({
+                filter: filter
+            }, () => {this.loadTagList()})
+        }
+    }
+
     render() {
         if(this.state.tagsLoaded === false) {
             return <div>There are no loaded tags yet!</div>
@@ -36,12 +92,21 @@ class TagOverview extends React.Component {
             const tags = this.state.tags
             const RenderTag = (tag) => {
                 const textColor = !tag.Hidden ? "text-white" : "text-blue-300"
+                const underline = this.indexOfTagInFilter(tag) != -1 ? " underline " : ""
                 return <div class="px-1">
-                    <div class={"bg-black border rounded " + textColor + " hover:text-black hover:bg-white cursor-pointer px-1 py-1"}>
+                    <div
+                        class={"bg-black border rounded " + textColor + underline + " hover:text-black hover:bg-white cursor-pointer px-1 py-1"}
+                        onClick = {() => this.switchFilter(tag)}
+                    >
                         #{tag.Name}
                     </div>
                 </div>
             }
+
+            // tags.filter(tag => tag.Hidden === false).map((tag, i) => {
+            //     return <div key={i} onClick={() => deleteCallback(tag)}>{RenderTag(tag)}</div>
+            // })
+
             return (
                 <div class={"flex flex-col items-center gap-2"}>
                     <div class="flex flex-row flex-wrap mr-0 ">
