f32a674ac04fc201f3d2fb52e0b56a2966768129
NotUnlikeTheWaves <hleistra@gmail.com>
2021-10-01 21:55:37 +0200

    Serve web app statically from golang

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.6.2
 	github.com/h2non/filetype v1.1.1
 	github.com/qiniu/qmgo v0.9.1
--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -25,5 +26,6 @@ func main() {
 	r.POST("/api/v1/document", apiUploadFiles)
 	r.DELETE("/api/v1/document/:fileName/tags", apiRemoveTags)
 	r.Static("/api/v1/content", "./documents")
+	r.Use(static.Serve("/", static.LocalFile("../web/build", true)))
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
